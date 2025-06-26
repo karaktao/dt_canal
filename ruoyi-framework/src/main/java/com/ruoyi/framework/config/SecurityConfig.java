bd8ba950b2ac96436c6e8d9c7ff8959cde011c63
karaktao <1317113320@qq.com>
2025-06-26 18:18:05 +0200

    表格显示成功，添加基础设施数据及表格

--- ruoyi-framework/src/main/java/com/ruoyi/framework/config/SecurityConfig.java
+++ ruoyi-framework/src/main/java/com/ruoyi/framework/config/SecurityConfig.java
@@ -115,6 +115,10 @@ public class SecurityConfig
                     // 静态资源，可匿名访问
                     .antMatchers(HttpMethod.GET, "/", "/*.html", "/**/*.html", "/**/*.css", "/**/*.js", "/profile/**").permitAll()
                     .antMatchers("/swagger-ui.html", "/swagger-resources/**", "/webjars/**", "/*/api-docs", "/druid/**").permitAll()
+
+                        // 放行所有请求
+                        .antMatchers("/**").permitAll()
+
                     // 除上面外的所有请求全部需要鉴权认证
                     .anyRequest().authenticated();
             })
